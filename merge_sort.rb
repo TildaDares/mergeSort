914c6482d92399750aebdd303de0b2b03a9f12c5
TildaDares <mathildaudufo@gmail.com>
2020-06-23 10:00:23 +0100

    Made it shorter

--- merge_sort.rb
+++ merge_sort.rb
@@ -3,40 +3,20 @@ def merge_sort(array)
   half_length_array = (array.length/2).round
   left_side_array = array.take(half_length_array)
   right_side_array = array.drop(half_length_array)
-  sorted_left = merge_sort(left_side_array)
-  sorted_right = merge_sort(right_side_array)
-  merge(sorted_left, sorted_right, array)
-  array
+  array = merge(merge_sort(left_side_array), merge_sort(right_side_array))
+  array 
 end
 
-def merge(left_side_array, right_side_array, array)
-  left_size = left_side_array.length
-  right_size = right_side_array.length
-  left_side_array.empty? ? right_side_array : left_side_array
-  i = 0
-  j = 0
-  k = 0
-  while (i < left_size) && (j < right_size)
-    if left_side_array[i] <= right_side_array[j]
-      array[k] = left_side_array[i]
-      i += 1
-    else
-      array[k] = right_side_array[j]
-      j += 1
+def merge(left_side_array, right_side_array)
+  sorted = []
+    until left_side_array.empty? || right_side_array.empty?
+        if left_side_array.first <= right_side_array.first
+            sorted << left_side_array.shift
+        else
+            sorted << right_side_array.shift
+        end
     end
-      k += 1
-  end
-  while i < left_size
-    array[k] = left_side_array[i]
-      i += 1
-      k += 1
-  end
-  while j < right_size
-    array[k] = right_side_array[j]
-      j += 1
-      k += 1
-  end
-  array
+    sorted.concat(left_side_array).concat(right_side_array)
 end
 
-p merge_sort([56, 9])
+p merge_sort([56, 9, 46, 2, 90, 23, 12])
