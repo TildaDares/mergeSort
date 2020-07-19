d9ef6fe0f90a6854054c3a9e2da478a5485cb7ba
Tilda Udufo <mathildaudufo@gmail.com>
2020-07-19 15:59:07 +0100

    Refactored

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal :true
+
 def merge_sort(array)
   return array if array.length < 2
-  half_length_array = (array.length/2).round
+
+  half_length_array = (array.length / 2).round
   left_side_array = array.take(half_length_array)
   right_side_array = array.drop(half_length_array)
   merge(merge_sort(left_side_array), merge_sort(right_side_array))
@@ -8,14 +11,14 @@ end
 
 def merge(left_side_array, right_side_array)
   sorted = []
-    until left_side_array.empty? || right_side_array.empty?
-        if left_side_array.first <= right_side_array.first
-            sorted << left_side_array.shift
-        else
-            sorted << right_side_array.shift
-        end
-    end
-    sorted.concat(left_side_array).concat(right_side_array)
+  until left_side_array.empty? || right_side_array.empty?
+    sorted << if left_side_array.first <= right_side_array.first
+                left_side_array.shift
+              else
+                right_side_array.shift
+                end
+  end
+  sorted.concat(left_side_array).concat(right_side_array)
 end
 
 p merge_sort([56, 9, 46, 2, 90, 23, 12])
